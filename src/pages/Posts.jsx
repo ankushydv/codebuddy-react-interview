a39b376a1a8b2143c1f1b704d5bf7f25997eb1b2
Praveen <>
2023-01-16 17:48:31 +0530

    set posts  length as screen length

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,18 +1,40 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { PostContext } from '../context/PostContext';
 
 const Posts = () => {
+  const [width, setWidth] = useState(window.innerWidth);
   const { posts } = useContext(PostContext);
+  const isLargerScreen = width > 1008;
+  const isMediaScreen = width < 1007 && width < 642;
+  const isSmallScreen = width > 641;
 
+  console.log('isLargerScreen', isLargerScreen, isMediaScreen, isSmallScreen);
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  });
+  const postsForLarge = isLargerScreen
+    ? posts.slice(0, 3)
+    : isMediaScreen
+    ? posts.slice(0, 2)
+    : posts.slice(0, 1);
+  console.log('postsForLargeScreen', postsForLarge);
   return (
     <Container>
       <h2 className="text-center mb-2">List of Posts</h2>
       <Container>
         {posts &&
-          Object.keys(posts).map(post => (
+          Object.keys(postsForLarge).map(post => (
             <Row>
               {' '}
               <Col className="md-4">
@@ -33,7 +55,7 @@ const Posts = () => {
                 </Row>{' '}
               </Col>
               <Col className="md-4">
-                <img src={posts[post].image} alt="img" width="100px" height="100px" />
+                <img src={posts[post].image} alt="img" width="160px" height="100px" />
               </Col>
               <Col className="md-4">
                 <img src={posts[post].avatar} alt="img2" width="100px" height="100px" />
